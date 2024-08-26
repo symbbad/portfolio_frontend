3a622a82fd5fc629ebe994e2f3a8752ddc640b99
symbbad <symbbad@gmail.com>
2024-08-26 18:06:43 +0900

    chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // Tailwind가 클래스를 감지할 파일 경로
+    './src/**/*.{js,jsx,ts,tsx}', // Tailwind CSS가 스타일을 적용할 파일 경로
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
