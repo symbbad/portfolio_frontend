8680509c63fcc123db7ce25caf5f7410cae975e4
symbbad <symbbad@gmail.com>
2024-08-29 20:06:45 +0900

    update: Change Style

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -8,7 +8,7 @@ const Content: React.FC = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>Symbbad</h1>
       <p className={styles.description}>
-        네트워크와 인프라 분야에서 끊임없이 탐구하고 가치를 창출하는, <span style={{ fontWeight: "900", color: "#6bdb6f", textShadow: "0 0 10px rgba(76, 175, 80, 0.8), 0 0 20px rgba(76, 175, 80, 0.6)" }}>기술 전문가</span>가 되고자 합니다
+        네트워크와 인프라 분야에서 끊임없이 탐구하고 가치를 창출하는,<br></br> <span style={{ fontWeight: "900", color: "#6bdb6f", textShadow: "0 0 10px rgba(76, 175, 80, 0.8), 0 0 20px rgba(76, 175, 80, 0.6)" }}>기술 전문가</span>가 되고자 합니다
       </p>
       <div className={styles.divider} />
       <ul className={styles.list}>
