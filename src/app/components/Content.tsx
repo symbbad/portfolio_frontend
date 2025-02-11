9e8587ac077084200b278c9584882e1de18fda26
symbbad <symbbad@gmail.com>
2025-02-11 10:54:38 +0900

    fea

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -8,7 +8,7 @@ const Content: React.FC = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>Simhyunjun</h1>
       <p className={styles.description}>
-        네트워크, 인프라 분야에서 끊임없이 탐구하고 가치를 창출하는,<br></br> <span style={{ fontWeight: "900", color: "#6bdb6f"}}>기술 전문가</span>를 목표합니다다
+        네트워크, 인프라 분야에서 끊임없이 탐구하고 가치를 창출하는 <span style={{ fontWeight: "900", color: "#6bdb6f"}}>기술 전문가</span>를 목표합니다
       </p>
       <div className={styles.divider} />
       <ul className={styles.list}>
